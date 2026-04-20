6a53744d43cc0e4bb7f32916005348b2f93efb7c
WesleyC115 <wesley.moura03@hotmail.com>
2026-04-20 19:32:06 -0300

    fix: package-lock atualizado

--- Back/src/main/java/com/example/Back/config/WebConfig.java
+++ Back/src/main/java/com/example/Back/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5500", "http://127.0.0.1:5500") // ADICIONE A URL DO REACT
+                .allowedOrigins("http://localhost:5173", "http://localhost:5500", "http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
